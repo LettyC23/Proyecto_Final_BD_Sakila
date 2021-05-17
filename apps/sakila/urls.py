57e7cb082480aab3630c1a109fca368e359b50da
LettyC23 <44488698+LettyC23@users.noreply.github.com>
2021-05-17 13:31:03 -0500

    editar actor

--- apps/sakila/urls.py
+++ apps/sakila/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
-from apps.sakila.views import crearActor, listarActor
+from apps.sakila.views import *
 
 urlpatterns = [
     path('crear_actor/', crearActor, name = 'crear_actor'),
-    path('listar_actor/', listarActor, name = 'listar_actor')
+    path('listar_actor/', listarActor, name = 'listar_actor'),
+    path('editar_actor/<int:actor_id>', editarActor, name = 'editar_actor'),
+    path('eliminar_actor/<int:actor_id>', eliminarActor, name = 'eliminar_actor')
 ]
--- apps/sakila/views.py
+++ apps/sakila/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import *
 from .models import *
 
@@ -18,3 +19,26 @@ def crearActor(request):
 def listarActor(request):
     actores = Actor.objects.all()
     return render(request, 'sakila/listar_actor.html',{'actores':actores})
+
+def editarActor(request, actor_id):
+    actor_form = None
+    error = None
+    try:
+        actor = Actor.objects.get(actor_id = actor_id)
+        if request.method == 'GET':
+            actor_form = ActorForm(instance = actor)
+        else:
+            actor_form =ActorForm(request.POST, instance = actor)
+            if actor_form.is_valid():
+                actor_form.save()
+            redirect('index')
+    except ObjectDoesNotExist as e:
+        error = e
+    return render(request,'sakila/crear_actor.html',{'actor_form':actor_form,'error':error})
+
+def eliminarActor (request, actor_id):
+    actor = Actor.objects.get(actor_id = actor_id)
+    if request.method == 'POST':
+        actor.delete()
+        return redirect('sakila:listar_actor')
+    return redirect(request,'sakila/eliminar_actor.html',{'actor':actor})
